d3071a32359570cc94b84acfa2a1c109689eb119
Momin Charles <charles.momin@student.uclouvain.be>
2015-04-27 23:41:30 +0200

    modif

--- CODE/programme.c
+++ CODE/programme.c
@@ -1,58 +1,52 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-
-void * importFile
+#include <semaphore.h>
+#include <pthread.h>
+#define TRUE 1
+#define FALSE 0
+//CHECK partout pour les boolean
 
 int main(int argc, const char *argv[]){
-//création des différents variables et outils
+
 int N;
-pthread_mutex_t nbrTab;
+int err;
+int i;
+
+pthread_mutex_t mutex1;
 sem_t empty1;
 sem_t full1;
-int err;
 long **tabNbr;
 
-//création des sémaphores et du mutex
-err=pthread_mutex_init(&nbrTab,NULL);
-if(err==0){
-	err=sem_init(&empty1,0,N);
-	if(err==0){
-		err=sem_init(&full1,0,0);
-	}
-}
-
-//analyse des paramètre au cas par cas
-int i;
+char *tabFile;
+char *tabUrl;
+int stdin_true = FALSE;
+//recherche des différents données sur les paramètres
 for(i=1;i<argc;i++){
-//on considère que cet argument est en premier.
-if(strcmp(argv[i],"-maxthreads")==0){
+	if(strcmp("-maxthreads",argv[i])==0)
 	N=atoi(argv[i+1]);
-	tabNbr = calloc(sizeof(*tabNbr));
-	tabNbr[0] = calloc(N*sizeof(long));
-	tabNbr[1] = calloc(N*sizeof(int));
+
+	
+	
 }
 
-if(strcmp(argv[i],"-stdin")==0){
-	printf("stdin repéré \n");
+
+
+//création des différents outils
+err=pthread_mutex_init(&mutex1,NULL);
+if(err==0){
+err = sem_init(&empty1,0,N);
+if(err==0){
+err = sem_init(&full1,0,0);
+}
 }
 
-if(strlen(argv[i])>13){
-	if(argv[i][4] == ':'){
-		printf("HTTP trouvée \n");
-	} else {
-		printf("fichier trouvé \n");
-	}
-} else {
-	printf("fichier trouver. NAME=%s\n",argv[i]);
-}
+//allocation de mémoire du tableau
+tabNbr = malloc(sizeof(*tabNbr));
+tabNbr[0]=calloc(N,sizeof(long));
+tabNbr[1]=calloc(N,sizeof(long));
 
-}
 
-pthread_t bolos;
-err=pthread_create(&bolos,NULL,&show,NULL);
+
 return(EXIT_SUCCESS);
 }
