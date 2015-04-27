12650438b8d1a71c6bde13ea6e48452dc149698f
Momin Charles <charles.momin@student.uclouvain.be>
2015-04-28 00:24:06 +0200

    modif programme.c

--- CODE/programme.c
+++ CODE/programme.c
@@ -7,6 +7,14 @@
 #define FALSE 0
 //CHECK partout pour les boolean
 
+void * importFromFile(char **tab){
+printf("OK\n");
+}
+
+struct tabArgThread1{
+	char **tab;
+};
+
 int main(int argc, const char *argv[]){
 
 int N;
@@ -18,11 +26,13 @@ sem_t empty1;
 sem_t full1;
 long **tabNbr;
 
-char *tabFile;
+const char **tabFile;
 int sizetabFile =0;
-char *tabUrl;
+const char **tabUrl;
 int sizetabUrl =0;
 int stdin_bool = FALSE;
+
+pthread_t file; 
 
 //recherche des différents données sur les paramètres
 for(i=1;i<argc;i++){
@@ -34,12 +44,33 @@ for(i=1;i<argc;i++){
 
 	if(strlen(argv[i])>13){ //nombre de caractère nécessaire pour une URL valide
 		if(argv[i][4]==':'){
-			
+			sizetabUrl++;
 		} else {
-			
+			sizetabFile++;
 		}
 	} else {
 		sizetabFile++;
+ 	}
+	
+}
+
+//création des tableau d'argument par type
+tabFile = malloc(sizetabFile*sizeof(char *));
+tabUrl = malloc(sizetabFile*sizeof(char *));
+int runnerf=0;
+int runnerurl=0;
+for(i=1;i<argc;i++){
+	if(strlen(argv[i])>13){ //nombre de caractère nécessaire pour une URL valide
+		if(argv[i][4]==':'){
+			tabFile[runnerf] = argv[i];
+			runnerf++;
+		} else {
+			tabUrl[runnerurl] = argv[i];
+			runnerurl++;
+		}
+	} else {
+		tabFile[runnerf] = argv[i];
+		runnerf++;
  	}
 	
 }
@@ -61,6 +92,9 @@ tabNbr[0]=calloc(N,sizeof(long));
 tabNbr[1]=calloc(N,sizeof(long));
 
 
+//lancement des threads de récupération
+
+err = pthread_create(&file, NULL,&importFromFile,(void *) tabFile);
 
 return(EXIT_SUCCESS);
 }
